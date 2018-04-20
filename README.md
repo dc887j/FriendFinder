e0494291ea595c57901f820fa0437554998b416a
Dorian Crawford <doriancrawford@Dorians-MacBook-Pro.local>
2018-04-20 09:24:31 -0400

    last minute commits

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# FriendFinder
+Fresh Prince Friend Finder
+
+This full-stack site will takes in results from your users' surveys, then compares their answers with those of "Fresh Prince of Bel-Air" television show. The app will then display the name and picture of the character who best matches with the user's answers. Answer the 10 questions to see which "Fresh Prince of Bel-Air" character you are most similar to.
+
+Each user's results are converted into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+With that done, the difference between current user's scores are compared against those from other users, question by question. The difference is added up to calculate the `totalDifference`which is then used to display the corresponding character picture.
---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+
+var characterArray = [
+  {
+    name: "Will",
+    photo:
+      "http://junkee.com/wp-content/uploads/2015/08/FRESHPRINCE.jpg",
+    scores: [1, 4, 5, 1, 3, 4, 5, 5, 1, 5]
+  },
+  {
+    name: "Carlton",
+    photo:
+      "http://images1.fanpop.com/images/photos/1900000/-will-smith-1942257-289-400.jpg",
+    scores: [1, 5, 2, 5, 1, 5, 2, 5, 5, 1]
+  },
+  {
+    name: "Uncle Phil",
+    photo:
+      "http://cimg.tvgcdn.net/i/r/2014/10/27/d4e10491-d3bc-4011-a4e4-17651e104ebe/thumbnail/624x463/d6e0e1c83f358ccc9a8c047df26fba65/fresh-prince-uncle-phil2.jpg",
+    scores: [4, 3, 1, 5, 1, 2, 1, 1, 5, 3]
+  },
+  {
+    name: "Aunt Viv",
+    photo:
+      "http://www.viralthread.com/wp-content/uploads/2017/03/aunt-viv-change.jpg",
+    scores: [3, 5, 3, 5, 2, 5, 1, 1, 3, 1]
+  },
+  {
+    name: "Jeffrey",
+    photo:
+      "http://introtomasscommfall11.wikispaces.com/file/view/Geoffrey.jpg",
+    scores: [5, 5, 5, 1, 5, 1, 1, 1, 5, 1]
+  }
+];
+
+module.exports = characterArray;
